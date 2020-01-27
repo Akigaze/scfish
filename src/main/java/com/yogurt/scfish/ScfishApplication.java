e75924a01006f9ae87fbe59a265e52028f902681
Akigaze <1175520196@qq.com>
2020-01-27 19:49:06 +0800

    init project

---
+++ src/main/java/com/yogurt/scfish/ScfishApplication.java
@@ -0,0 +1,13 @@
+package com.yogurt.scfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScfishApplication.class, args);
+	}
+
+}
