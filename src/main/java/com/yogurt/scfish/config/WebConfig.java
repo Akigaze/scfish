a8ba2647a75c3fd13265ed7863ff34599cdbfa4b
Akigaze <1175520196@qq.com>
2020-01-28 16:01:00 +0800

    config web interceptor and view controller

---
+++ src/main/java/com/yogurt/scfish/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.yogurt.scfish.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  // 跳转的映射配置
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/login").setViewName("login");
+    registry.addViewController("/register").setViewName("register");
+  }
+
+  // 配置资源文件的映射路径
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    // registry.addResourceHandler("/demo/**").addResourceLocations("classpath:/");
+  }
+
+  // 配置请求拦截器
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new WebInterceptor())
+        .addPathPatterns("/**")
+        .excludePathPatterns("/", "/login", "/register");
+  }
+}
---
+++ src/main/java/com/yogurt/scfish/config/WebInterceptor.java
@@ -0,0 +1,32 @@
+package com.yogurt.scfish.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class WebInterceptor implements HandlerInterceptor {
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    System.out.println("--- preHandle ---");
+    HttpSession session = request.getSession(true);
+    if (session.getAttribute("user-id") == null){
+      response.sendRedirect("/login");
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+  }
+}
