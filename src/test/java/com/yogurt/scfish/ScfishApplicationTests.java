04daddabb49b6875b5ea5fc2ee0b7952c6ff4c01
Akigaze <1175520196@qq.com>
2020-01-30 14:51:41 +0800

    fix unit test failure

--- src/test/java/com/yogurt/scfish/ScfishApplicationTests.java
+++ src/test/java/com/yogurt/scfish/ScfishApplicationTests.java
@@ -1,17 +1,11 @@
 package com.yogurt.scfish;
 
 
-import com.yogurt.scfish.service.PostService;
-import lombok.AllArgsConstructor;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ScfishApplicationTests {
-
-	PostService postService;
-
 	@Test
 	void contextLoads() {
 	}
